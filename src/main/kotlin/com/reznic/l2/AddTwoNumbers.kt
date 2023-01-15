a19c4864c7e4e125db5e7d62b1b87372ea5d5163
alexandr reznic <reznic.a@gmail.com>
2023-01-15 17:32:41 +0200

    add link to leetcode

--- src/main/kotlin/com/reznic/l2/AddTwoNumbers.kt
+++ src/main/kotlin/com/reznic/l2/AddTwoNumbers.kt
@@ -2,47 +2,13 @@ package com.reznic.l2
 
 import java.math.BigInteger
 
+
 /**
- * https://leetcode.com/problems/add-two-numbers/
+ * @see <a href="https://leetcode.com/problems/add-two-numbers/">leetcode.com/add-two-numbers</a>
+ *<br>
  * 2. Add Two Numbers - Kotlin
  */
-fun main() {
-    val solution = Solution()
-    val case0 = solution
-        .addTwoNumbers(
-            solution.createListNode(intArrayOf(5,6,4)),
-            solution.createListNode(intArrayOf(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1))
-        )
-
-    println(case0)
-
-    val case2 = solution
-        .addTwoNumbers(
-            solution.createListNode(intArrayOf(2, 4, 9)),
-            solution.createListNode(intArrayOf(5, 6, 4, 9))
-        )
-    //[7,0,4,0,1]
-    println(case2)
-
-    val case4 = solution
-        .addTwoNumbers(
-            solution.createListNode(intArrayOf(  9)),
-            solution.createListNode(intArrayOf(1,9,9,9,9,9,9,9,9,9))
-        )
-     println(case4)
-
-    val case3 = solution
-        .addTwoNumbers(
-            solution.createListNode(intArrayOf(9, 9, 9, 9, 9, 9, 9)),
-            solution.createListNode(intArrayOf(9, 9, 9, 9))
-        )
-
-    println(case3)
-}
-
-
 class Solution {
-
     fun createListNode(values: IntArray): ListNode {
         val root = ListNode(values.first())
         var next: ListNode = root
@@ -69,15 +35,83 @@ class Solution {
             next = next?.next
             next2 = next2?.next
         }
-        val firstNumber = BigInteger(l1A.reversed().joinToString(separator = "") { it.toString(10) },10)
-        val secondNumber = BigInteger(l2A.reversed().joinToString(separator = "") { it.toString(10) },10)
+        val firstNumber = BigInteger(l1A.reversed().joinToString(separator = "") { it.toString(10) }, 10)
+        val secondNumber = BigInteger(l2A.reversed().joinToString(separator = "") { it.toString(10) }, 10)
         val sumString = (secondNumber + firstNumber).toString()
-        val resultList  = sumString.map {
+        val resultList = sumString.map {
             it.toString().toInt(10)
         }.reversed()
 
         return createListNode(resultList.toIntArray())
     }
+}
+
+fun main() {
+    val solution = Solution()
+    val case0 = solution
+        .addTwoNumbers(
+            solution.createListNode(intArrayOf(5, 6, 4)),
+            solution.createListNode(
+                intArrayOf(
+                    1,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1
+                )
+            )
+        )
+
+    println(case0)
+
+    val case2 = solution
+        .addTwoNumbers(
+            solution.createListNode(intArrayOf(2, 4, 9)),
+            solution.createListNode(intArrayOf(5, 6, 4, 9))
+        )
+    //[7,0,4,0,1]
+    println(case2)
+
+    val case4 = solution
+        .addTwoNumbers(
+            solution.createListNode(intArrayOf(9)),
+            solution.createListNode(intArrayOf(1, 9, 9, 9, 9, 9, 9, 9, 9, 9))
+        )
+    println(case4)
+
+    val case3 = solution
+        .addTwoNumbers(
+            solution.createListNode(intArrayOf(9, 9, 9, 9, 9, 9, 9)),
+            solution.createListNode(intArrayOf(9, 9, 9, 9))
+        )
+
+    println(case3)
 }
 
 data class ListNode(var `val`: Int) {
